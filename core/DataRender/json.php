e0015ace4190fdb1e9acc45f56ecd24d08caa21e
Paul <ptarcher@gmail.com>
2010-06-01 18:58:43 +1000

    Add the correct headers before sending the JSON.

--- core/DataRender/json.php
+++ core/DataRender/json.php
@@ -4,7 +4,11 @@ require_once('core/DataRender.php');
 
 class CoreDataRender_Json extends CoreDataRender {
     function render() {
-        // TODO: Set the headers correctly
+        // Set the headers correctly
+        header('Content-Type: text/javascript; charset=utf8');
+        header('Access-Control-Allow-Origin: http://www.example.com/');
+        header('Access-Control-Max-Age: 3628800');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
         return json_encode($this->data);
     }
 }
