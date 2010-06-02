4892f7325ca56739b01fb95b85510fbc20e60c13
Paul <ptarcher@gmail.com>
2010-06-02 12:43:20 +1000

    Updated to graph with jqplot.

--- modules/SessionGraphs/templates/sessiongraphs.js
+++ modules/SessionGraphs/templates/sessiongraphs.js
@@ -2,48 +2,140 @@ $(function () {
     // Call the Open Streetmap API
     //OSM_Init();
     // Call the googlemaps API
-    //GoogleMaps_Init();
+    GoogleMaps_Init();
 
-    var options = {
-        lines: { show: true },
-        //points: { show: true },
-        xaxis: { mode: "time", timeformat: "%H:%M:%S" }
+    // jqplot
+    var jqplot_options = {
+        title:session_date,
+        seriesDefaults:{showMarker: false},
+        /*
+        series:[
+            {label:'Heart Rate', }, 
+            {label:'Speed', yaxis:'y2axis'}, 
+        ],*/
+        axesDefaults:{
+            //tickOptions:{formatString:"%d"}, 
+            autoscale:true, 
+            useSeriesColor:true,
+        },
+        axes:{
+            xaxis:{
+                renderer:$.jqplot.DateAxisRenderer,
+                tickOptions:{formatString:'%H:%M:%S'},
+                label:'Time',
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+            },
+            yaxis:{
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+            },
+                  /*
+            y2axis:{
+                label:'Speed',
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+            },*/
+        },
+        highlighter: {
+            sizeAdjust: 10,
+            tooltipLocation: 'n',
+            tooltipAxes: 'y',
+            tooltipFormatString: '%.2f',
+            useAxesFormatters: false
+        },
+        cursor: {
+            show:true, 
+            zoom:true
+        },
+
+        //legend: {location:'ne'},
     };
-    var data = [];
-    var placeholder = $("#placeholder");
-    
-    $.plot(placeholder, data, options);
 
-    // fetch one series, adding to what we got
-    var alreadyFetched = {};
-    
-    // then fetch the data with jQuery
-    function onDataReceived(series) {
-        // let's add it to our current data
-        if (!alreadyFetched[series.label]) {
-            alreadyFetched[series.label] = true;
-            data.push(series);
-        }
-
-        // and plot all we got
-        $.plot(placeholder, data, options);
-    }
-        
-    $.ajax({
-        url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=speed",
-        method: 'GET',
-        dataType: 'json',
-        success: onDataReceived
-    });
-
-    // TODO: The session date needs to be dynamic */
     $.ajax({
         url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=heartrate",
         method: 'GET',
         dataType: 'json',
-        success: onDataReceived
+        success: function (series) {
+                     var options = jqplot_options;
+                     options.title = 'Heart Rate';
+                     options.axes.yaxis.label = 'Heart Rate (bpm)';
+                     $.jqplot('heartrate', [series], options);
+                 }
     });
 
-    GoogleMaps_Init();
+    
+    $.ajax({
+        url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=speed",
+        method: 'GET',
+        dataType: 'json',
+        success: function (series) {
+                    var options = jqplot_options;
+                    options.title = 'Speed';
+                    options.axes.yaxis.label = 'Speed (km/h)';
+                    $.jqplot('speed', [series], options);
+                 }
+    });
+
+
+    /*
+    $.ajax({
+        url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=distance",
+        method: 'GET',
+        dataType: 'json',
+        success: function (series) {
+                     var options = jqplot_options;
+                     options.title = 'Distance';
+                     options.axes.yaxis.label = 'Distance (km)';
+                     $.jqplot('distance', [series], options);
+                 }
+
+    });*/
+
+    $.ajax({
+        url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=altitude",
+        method: 'GET',
+        dataType: 'json',
+        success: function (series) {
+                     var options = jqplot_options;
+                     options.title = 'Altitude';
+                     options.axes.yaxis.label = 'Elevation (m)';
+                     $.jqplot('altitude', [series], options);
+                 }
+
+    });
+
+    $.ajax({
+        url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=cadence",
+        method: 'GET',
+        dataType: 'json',
+        success: function (series) {
+                     var options = jqplot_options;
+                     options.title = 'Cadence';
+                     options.axes.yaxis.label = 'Cadence (rpm)';
+                     $.jqplot('cadence', [series], options);
+                 }
+    });
+
+    $.ajax({
+        url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=power",
+        method: 'GET',
+        dataType: 'json',
+        success: function (series) {
+                     var options = jqplot_options;
+                     options.title = 'Power';
+                     options.axes.yaxis.label = 'Power (watts)';
+                     $.jqplot('power', [series], options);
+                 }
+    });
+
+    $.ajax({
+        url: "index.php?module=APIAccess&method=SessionGraphs.getSessionDataField&session_date="+encodeURIComponent(session_date)+"&field=temperature",
+        method: 'GET',
+        dataType: 'json',
+        success: function (series) {
+                     var options = jqplot_options;
+                     options.title = 'Temperature';
+                     options.axes.yaxis.label = 'Temperature (C)';
+                     $.jqplot('temperature', [series], options);
+                 }
+    });
 });
 
