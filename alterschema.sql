65a6633f563e64dcfaef6e2eb97865e97bdb6312
Paul <ptarcher@gmail.com>
2010-06-02 12:41:59 +1000

    Add the missing data fields.

---
+++ alterschema.sql
@@ -0,0 +1,4 @@
+ALTER TABLE t_exercise_data ADD COLUMN temperature numeric;
+ALTER TABLE t_exercise_data ADD COLUMN cadence     numeric;
+ALTER TABLE t_exercise_data ADD COLUMN power       numeric;
+ALTER TABLE t_exercise_data ADD COLUMN altitude    numeric;
