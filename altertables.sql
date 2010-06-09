20ef61073d72cb28d4d3c9d8f20379fdc0637ca8
Paul Archer <ptarcher@gmail.com>
2010-06-10 08:40:49 +1000

    Remove tables that have been modified.

--- altertables.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE t_users ADD COLUMN athlete     boolean NOT NULL DEFAULT true;
-ALTER TABLE t_users ADD COLUMN coach       boolean NOT NULL DEFAULT false;
-ALTER TABLE t_users ADD COLUMN "superuser" boolean NOT NULL DEFAULT false;
-
-UPDATE t_users SET superuser = 't' WHERE userid = 'ptarcher';
