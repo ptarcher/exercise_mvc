44ccf58ade5cc47c8215493463a6b226c75338c8
Paul Archer <ptarcher@gmail.com>
2011-02-18 00:59:34 +1100

    Add a User class

---
+++ Core/User.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ *  Description: 
+ *  Date:        18/02/2011
+ *  
+ *  Author:      Paul Archer <ptarcher@gmail.com>
+ *
+ * Copyright (C) 2011  Paul Archer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Core_User {
+	static private $instance = null;
+    var $user;
+
+	/**
+     */
+    function __construct() {
+        $this->user = new Zend_Session_Namespace('user');
+    }
+
+	/**
+     */
+	static public function getInstance()
+    {
+		if (self::$instance == null)
+        {
+			$c = __CLASS__;
+			self::$instance = new $c;
+		}
+		return self::$instance;
+    }
+
+	/**
+     */
+	static public function getUserId()
+    {
+        $me = self::getInstance();
+        return $me->user->userid;
+    }
+
+	/**
+     */
+    function __destruct() {
+        unset($this->instance);
+        unset($this->user);
+    }
+}
+
+?>
