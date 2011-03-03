ecf4f0f0278413a03e39d35c9647a43ffb42db94
Paul Archer <ptarcher@gmail.com>
2011-03-03 22:10:31 +1100

    Add some AJAX to get the weekly plans

--- Module/DashBoard/templates/dashboard.js
+++ Module/DashBoard/templates/dashboard.js
@@ -1,12 +1,23 @@
 $(document).ready( function() {
-    today = new Date();
+    //var monday = Date.parse('monday').toString('yyyy-MM-dd');
+    var monday = Date.parse('2010-06-14').toString('yyyy-MM-dd');
 
-    var week   = today.getWeekOfYear();
+    $.ajax({
+        url: "index.php?"+
+                        "module=APIAccess"+
+                        "&method=Plans.getDailyPlans"+
+                        "&week_date="+encodeURIComponent(monday),
+        method: 'GET',
+        dataType: 'json',
+        success: function (weeks, textStatus, XMLHttpRequest) {
+            $('div#week_of_year').html(weeks);
+        }
+    });
+
+
     /* Start of week is monday, end of week is sunday */
-    var monday = Date.parse('monday').toISOString();
     var sunday = Date.parse('next sunday').toString('yyyy-M-d');
 
-    $('div#week_of_year').html('week of year = ' + week);
     $('div#monday').html(monday);
     $('div#sunday').html(sunday);
 });
