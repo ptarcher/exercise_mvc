fadab846098d2e707eb44eff2d3c95d06abee833
Paul <ptarcher@gmail.com>
2011-03-22 08:44:19 +1100

    Add the column session_timestamp to each plan

--- altertables.sql
+++ altertables.sql
@@ -288,3 +288,5 @@ UPDATE t_exercise_laps SET total_duration = duration;
 
 ALTER TABLE t_users ADD COLUMN dob date;
 ALTER TABLE t_users ADD COLUMN token character varying(64) NOT NULL DEFAULT 'abc';
+ALTER TABLE t_exercise_plans_daily ADD COLUMN session_timestamp timestamp with time zone;
+ALTER TABLE t_exercise_plans_daily ADD CONSTRAINT t_users_zones_fk_sessions FOREIGN KEY (status) REFERENCES t_exercise_totals (session_date,userid) ON UPDATE NO ACTION ON DELETE NO ACTION;
