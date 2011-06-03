8aee8f5a16a56b7377017e58f49711f7781adff7
Paul Archer <ptarcher@gmail.com>
2011-06-03 23:02:39 +1000

    Convert all to option to disable internet required resources

--- Module/SessionGraphs/templates/sessionlaps.js
+++ Module/SessionGraphs/templates/sessionlaps.js
@@ -1,13 +1,16 @@
 $(function () {
-    // Call the Open Streetmap API
-    //OSM_Init();
-    // Call the googlemaps API
-    GoogleMaps_Init("index.php?module=APIAccess&"+
-                                "method=SessionGraphs.getGPXData&"+
-                                "format=gpx&"+
-                                "session_date="+encodeURIComponent(session_date)+"&"+
-                                "min_time="+encodeURIComponent(min_time)+"&"+
-                                "max_time="+encodeURIComponent(max_time));
+    if (MapData == 'openstreetmap') {
+        // Call the Open Streetmap API
+        OSM_Init();
+    } else if (MapData == 'google') {
+        // Call the googlemaps API
+        GoogleMaps_Init("index.php?module=APIAccess&"+
+                                  "method=SessionGraphs.getGPXData&"+
+                                  "format=gpx&"+
+                                  "session_date="+encodeURIComponent(session_date)+"&"+
+                                  "min_time="+encodeURIComponent(min_time)+"&"+
+                                  "max_time="+encodeURIComponent(max_time));
+    }
 
     // jqplot
     var jqplot_options = {
