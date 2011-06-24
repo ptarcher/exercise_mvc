68decf4a75c934b3194797a079116989b02c81e9
Paul Archer <ptarcher@gmail.com>
2011-06-24 14:36:48 +1000

    phpcs

--- Module/UserManagement/templates/usersettings.js
+++ Module/UserManagement/templates/usersettings.js
@@ -1,20 +1,18 @@
 function getUpdateUserSettingAJAX( id, value )
 {
-	var ajaxRequest = coreHelper.getStandardAjaxConf();
-	coreHelper.toggleAjaxLoading();
-	
+    var ajaxRequest = coreHelper.getStandardAjaxConf();
+    coreHelper.toggleAjaxLoading();
+
     // TODO, Add all the session paramaters
-	var parameters    = {};
+    var parameters    = {};
     parameters.module = 'APIAccess';
     parameters.format = 'json';
     parameters.method = 'UserManagement.updateSetting';
-
     parameters.id     = id;
     parameters.value  = value;
+    ajaxRequest.data  = parameters;
 
-	ajaxRequest.data = parameters;
-	
-	return ajaxRequest;
+    return ajaxRequest;
 }
 
 
@@ -23,49 +21,52 @@ $(document).ready( function() {
             widgets: ['zebra',],
     });
     
-	var alreadyEdited = new Array;
-	$('.settings').click( function() {
-			$(this).toggle().parent()
-				.prepend( $('<img src="themes/default/images/ok.png" class="update">')
-							.click( function(){ $.ajax( getUpdateSettingAJAX( $('tr#'+idRow) ) ); } ) 
-					);
-		}
-	);
-	
-	$('td.editable').click( function() { 
-			coreHelper.ajaxHideError();
-			var idRow = $(this).attr('id');
-			if(alreadyEdited[idRow]==1) return;
-			alreadyEdited[idRow] = 1;
+    var alreadyEdited = new Array;
+    $('.settings').click( function() {
+        $(this).toggle().parent()
+               .prepend( $('<img src="themes/default/images/ok.png" class="update">').click( function(){ $.ajax( getUpdateSettingAJAX( $('tr#'+idRow) ) ); } ) 
+           );
+    });
 
-            var id             = 'edit_'+$(this).attr('id');
-            var content_before = $(this).html();
-            if ($(this).attr('id') == 'password') {
-                var content_after  = '<input id="'+id+'" type=password value="">';
-            } else {
-                var content_after  = '<input id="'+id+'" type=text value="'+content_before+'">';
-            }
-            $(this).html(content_after)
-                   .keypress(submitSessionOnEnter);
+    $('td.editable').click( function() { 
+        var idRow = $(this).attr('id');
 
-            $("input#edit_dob").datepicker({
-                changeMonth: true,
-                changeYear: true,
-                dateFormat: 'dd/mm/yy',
-            });
-    } );
+        coreHelper.ajaxHideError();
+        if (alreadyEdited[idRow]==1) {
+            return;
+        }
+        alreadyEdited[idRow] = 1;
+
+        var id             = 'edit_'+$(this).attr('id');
+        var content_before = $(this).html();
+
+        if ($(this).attr('id') == 'password') {
+            var content_after = '<input id="'+id+'" type=password value="">';
+        } else {
+            var content_after = '<input id="'+id+'" type=text value="'+content_before+'">';
+        }
+
+        $(this).html(content_after)
+               .keypress(submitSessionOnEnter);
+
+        $("input#edit_dob").datepicker({
+                            changeMonth: true,
+                            changeYear: true,
+                            dateFormat: 'dd/mm/yy',
+});
+} );
 });
 
 function submitSessionOnEnter(e)
 {
-	var key=e.keyCode || e.which;
-	if (key==13)
-	{
+    var key = e.keyCode || e.which;
+
+    if (key==13) {
         var id    = $(this).attr('id');
         var value = $(this).find('input#'+'edit_'+id).val();
 
         $.ajax(getUpdateUserSettingAJAX(id, value));
-		//$(this).parent().find('.updateSession').click();
-		//$(this).find('.addsession').click();
-	}
+        //$(this).parent().find('.updateSession').click();
+        //$(this).find('.addsession').click();
+    }
 }
