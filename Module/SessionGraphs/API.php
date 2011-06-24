e20d5d4c8c49d4fe723a0ed592710d6c0468e458
Paul Archer <ptarcher@gmail.com>
2011-06-24 12:42:13 +1000

    phpcs

--- Module/SessionGraphs/API.php
+++ Module/SessionGraphs/API.php
@@ -1,42 +1,60 @@
 <?php
-/*
- *  Description: Display simple single digits of the current weather.
- *  Date:        02/06/2009
- *  
- *  Author:      Paul Archer <ptarcher@gmail.com>
+/**
+ * File short description.
  *
- * Copyright (C) 2009  Paul Archer
- * 
+ * PHP version 5
+ *
+ * @category  Bike
+ * @package   SessionGraphs
+ * @author    Paul Archer <ptarcher@gmail.com>
+ * @copyright 2009 Paul Archer
+ * @license   http://www.gnu.org/licenses/agpl-3.0.txt AGPL-3 .0
+ * @version   Release: 1.0
+ * @link      http://paul.archer.tw
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
-
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
-class Module_SessionGraphs_API extends Core_ModuleAPI {
-	static private $instance = null;
-	/**
-	 * Returns the singleton ModuleSessionGraphsAPI
-	 *
-	 * @return ModuleSessionGraphsAPI
-	 */
-	static public function getInstance()
-	{
-		if (self::$instance == null)
-		{			
-			$c = __CLASS__;
-			self::$instance = new $c();
-		}
-		return self::$instance;
-	}
+/**
+ * Class short description.
+ *
+ * Class long description.
+ *
+ * @category Bike
+ * @package  SessionGraphs
+ * @author   Paul Archer <ptarcher@gmail.com>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.txt AGPL-3 .0
+ * @link     http://paul.archer.tw
+ */
+class Module_SessionGraphs_API extends Core_ModuleAPI
+{
+    static private $instance = null;
+    /**
+     * Returns the singleton ModuleSessionGraphsAPI
+     *
+     * @return ModuleSessionGraphsAPI
+     */
+    static public function getInstance()
+    {
+        if (self::$instance == null)
+        {			
+            $c = __CLASS__;
+            self::$instance = new $c();
+        }
+        return self::$instance;
+    }
 	
     function getSessionDataField($session_date, $field, $min_time = NULL, $max_time = NULL) 
     {
